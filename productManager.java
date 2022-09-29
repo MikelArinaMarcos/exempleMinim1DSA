9c629eea87376648574442da6f5c2695a40dc038
PabloGarciaCalder <pablo-.maria.garcia@estudiantat.upc.edu>
2022-09-29 13:22:15 +0200

    primer commit

--- productManager.java
+++ productManager.java
@@ -1,2 +1,13 @@
+import src.producte;
+import java.util.Arrays;
+
+
 public class productManager {
+    public static void main (String [] args){
+
+    }
+
+    public static void sort (producte[] t){
+        Arrays.sort(t);
+    }
 }
