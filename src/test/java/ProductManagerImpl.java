dad9f8411881e184bfe8921c56e9164bd2c90346
PabloGarciaCalder <pablo-.maria.garcia@estudiantat.upc.edu>
2022-10-04 20:33:12 +0200

    he añadido algunas cosas que se explicaron en clase, pero no esta todo aun

--- src/test/java/ProductManagerImpl.java
+++ src/test/java/ProductManagerImpl.java
@@ -1,21 +1,54 @@
 import models.Order;
 import models.Product;
-import java.util.List;
+import models.User;
+
+import java.io.ObjectInputStream;
+import java.util.*;
 
 public class ProductManagerImpl implements ProductManager {
+
+    List<Product> products;
+    //private final ArrayList<Object> products;
+    HashMap<String, User> users;
+    //private final HashMap<Object, Object> users;
+    Queue<Order> orders;
+    //private final LinkedList<Object> orders; //--> esto es lo que me ponia el intellij para
+    //solucionar los errores, pero lo de arriba es lo que tenia el profe y ns que diferencia hay
+
+    public ProductManagerImpl(){
+        this.products = new ArrayList<>();
+        this.users = new HashMap<>();
+        this.orders = new LinkedList<>();
+    }
+
     @Override
     public List<Product> productsByPrice() {
-        return null;
+
+        List<Product> list = new ArrayList<>(this.products);
+        list.sort((Product p1, Product p2) -> Double.compare(p1.getPrice(), p2.getPrice()));
+
+        list.sort(new Comparator<Product>() {
+            @Override
+            public int compare(Product p1, Product p2) {
+                return Double.compare(p1.getPrice(), p2.getPrice());
+            }
+
+        });
+
+        return list;
     }
 
     @Override
     public List<Product> productsBySales() {
-        return null;
+        List<Product> list = new ArrayList<>(this.products);
+        list.sort((Product p1, Product p2) -> Double.compare(p1.getNumSales(), p2.getNumSales()));
+
+        return list;
     }
 
     @Override
     public void addOrder(Order order) {
-
+        this.orders.add(order);
     }
 
     @Override
